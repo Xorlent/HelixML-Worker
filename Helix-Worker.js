01d23a2226314577dbfb04ce83cd4045f44cf131
Xorlent <94985297+Xorlent@users.noreply.github.com>
2024-02-11 13:19:54 -0800

    Starter CloudFlare Worker HelixML Script

---
+++ Helix-Worker.js
@@ -0,0 +1,35 @@
+export default {
+  async fetch(request, env) {
+    let helixPrompt = 'What did you learn from the fine-tuning data?';
+    const auth = 'Bearer ' + env.helixauth;
+    const sessionID = 'YourSessionIDHere';
+    const helixURL = 'https://app.tryhelix.ai/api/v1/sessions/chat';
+
+    let helixHeaders = {
+      method: "POST",
+      headers: {
+        "Authorization": auth,
+        "Accept": "application/json",
+        //"Content-Type": "application/text",
+      },
+      body: JSON.stringify({
+        "model": "mistralai/Mistral-7B-Instruct-v0.1",
+        "stream": false,
+        "session_id": sessionID,
+        "messages": [
+            {
+                "role": "user",
+                "content": {
+                    "content_type": "text",
+                    "parts": [helixPrompt]
+                }            
+            }
+        ]
+      }),
+    };
+
+    let helixResponse = await fetch(helixURL,helixHeaders);
+    let helixBody = await helixResponse.body;
+    return new Response(helixBody, {status: 200});
+  },
+};
